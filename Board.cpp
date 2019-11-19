df4f053e3d9448551a550f8e3690c4aca80f92e9
Eklerek <mwiczuk99@gmail.com>
2019-11-19 20:33:09 +0100

    Added fruit spawn, fruit and snake timers

--- Board.cpp
+++ Board.cpp
@@ -1,11 +1,14 @@
 #include "Board.h"
 #include "Constants.h"
+#include <QtDebug>
+#include <math.h>
 
 Board::Board()
 {
     snake = new Snake();
     scene = new QGraphicsScene();
-
+    spawnTimer = new QTimer();
+    snakeTimer = new QTimer();
 
     setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
     setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
@@ -16,6 +19,97 @@ Board::Board()
     scene->addItem(snake);
     snake->Setup();
 
+    //scene->addItem(fruit);
+    //fruit->Spawn();
 
+    connect(spawnTimer, SIGNAL(timeout()), this, SLOT(SpawnFruit()));
+    spawnTimer->start(Data::FruitSpawnTime);
 
+    connect(snakeTimer, SIGNAL(timeout()), this, SLOT(MoveSnake()));
+    snakeTimer->start(Data::SnakeLatencySpeed);
+
+    /*if(timer->isActive())
+    {
+        qDebug() << "timer started";
+    }*/
 }
+
+
+
+void Board::SpawnFruit()
+{
+    if(Fruits.count() >= Data::MaxFruitNumber)
+    {
+        return;
+    }
+    Fruits.push_back(new Fruit{});
+    scene->addItem(Fruits.last());
+    Fruits.last()->Spawn();
+    //qDebug() << "Owoc: " << Fruits.last()->x() << ", " << Fruits.last()->y();
+}
+
+void Board::MoveSnake()
+{
+    snake->setFocus();
+    CheckCollision();
+    if(WallHit())
+    {
+        qDebug() << "Timer stopped";
+        snakeTimer->stop();
+    }
+    snake->setPos(snake->x() + snake->GetXDirection(), snake->y() + snake->GetYDirection());
+}
+
+void Board::CheckCollision()
+{
+    for(auto i = 0; i < Fruits.count(); ++i)
+    {
+        if(ComparePositions(Fruits[i]->x(), Fruits[i]->y(), snake->x(), snake->y()))
+        {
+            qDebug() << "Fruit met";
+        }
+    }
+}
+
+bool Board::ComparePositions(const double fX, const double fY, const double sX, const double sY) const
+{
+    if(fX == sX && fY == sY)
+    {
+qDebug() << "Pos compared";
+        return true;
+    }
+
+    return false;
+}
+
+bool Board::WallHit()
+{
+    if(snake->x() > Data::width/2|| x() < -Data::width/2|| y() > Data::height/2 || y() < -Data::height/2)
+    {
+        qDebug() << "Wall hit";
+        return true;
+    }
+    return false;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Board.h
+++ Board.h
@@ -1,16 +1,39 @@
 #pragma once
 #include <QGraphicsView>
 #include <QGraphicsObject>
-#include "Snake.h"
 #include <QWidget>
+#include <QVector>
+#include <QMainWindow>
+#include "Snake.h"
+#include "Fruit.h"
+
 class Board : public QGraphicsView
 {
+    Q_OBJECT
 public:
     Board();
+    ~Board()
+    {
+        delete scene;
+        delete snake;
+        delete spawnTimer;
+        delete snakeTimer;
+        // delete fruits
+    }
 
+public slots:
+    void SpawnFruit();
+    void MoveSnake();
 
 private:
     QGraphicsScene* scene;
     Snake* snake;
+    QTimer* spawnTimer;
+    QTimer* snakeTimer;
+    QVector<Fruit*> Fruits;
+
+    bool WallHit();
+    void CheckCollision();
+    bool ComparePositions(const double, const double, const double, const double) const;
 
 };
