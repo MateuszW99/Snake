667f98490e08deed3bfde6e29374fd6d21082d7b
Eklerek <mwiczuk99@gmail.com>
2019-11-19 20:32:36 +0100

    More constants

--- Constants.h
+++ Constants.h
@@ -1,9 +1,13 @@
 #pragma once
+#include <QColor>
 
 namespace Data {
     constexpr int width = 800;
     constexpr int height = 600;
     constexpr int yVelocity = 4;
     constexpr int xVelocity = 4;
-
+    constexpr int FruitSpawnTime = 2500;
+    constexpr int SnakeLatencySpeed = 50;
+    constexpr int MaxFruitNumber = 3;
+    constexpr double Epsilon = 0.00001;
 }
