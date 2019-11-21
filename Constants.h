54b66f99fa813e6827ebce5d04be36fa5751283b
Eklerek <mwiczuk99@gmail.com>
2019-11-21 22:36:03 +0100

    Added snake sizes

--- Constants.h
+++ Constants.h
@@ -2,13 +2,18 @@
 #include <QColor>
 
 namespace Data {
+    // Window
     constexpr int width = 800;
     constexpr int height = 600;
+    // Snake
+    constexpr int snakeSize = 10;
+    constexpr qreal snakeRadius = 3.0;
     constexpr int yVelocity = 4;
     constexpr int xVelocity = 4;
     constexpr int FruitSpawnTime = 2500;
-    constexpr int SnakeLatencySpeed = 50;
-    constexpr int MaxFruitNumber = 3;
-    constexpr int FruitSize = 10;
-    constexpr qreal FruitRadius = 3.0;
+    constexpr int SnakeLatencySpeed = 50;    
+    //Fruit
+    constexpr int maxFruitNumber = 3;
+    constexpr int fruitSize = 10;
+    constexpr qreal fruitRadius = 3.0;
 }
