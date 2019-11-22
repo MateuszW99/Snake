79c7c4b4ca297f27ebc2c402e3475f4afbff7834
Eklerek <mwiczuk99@gmail.com>
2019-11-23 00:15:44 +0100

    Changed furit spawning speed to 3s

--- Constants.h
+++ Constants.h
@@ -1,5 +1,5 @@
 #pragma once
-#include <QColor>
+#include <QObject>
 
 namespace Data {
     // Window
@@ -10,10 +10,10 @@ namespace Data {
     constexpr qreal snakeRadius = 3.0;
     constexpr int yVelocity = 4;
     constexpr int xVelocity = 4;
-    constexpr int FruitSpawnTime = 2500;
     constexpr int SnakeLatencySpeed = 50;    
     //Fruit
-    constexpr int maxFruitNumber = 3;
+    constexpr int FruitSpawnTime = 3000;
+    constexpr int maxFruitNumber = 4;
     constexpr int fruitSize = 10;
     constexpr qreal fruitRadius = 3.0;
 }
