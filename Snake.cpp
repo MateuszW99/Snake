1830d64219b788f2cc2e6d46d573c802ae633ea1
Eklerek <mwiczuk99@gmail.com>
2019-11-25 01:49:45 +0100

    Snake tail moves properly

--- Snake.cpp
+++ Snake.cpp
@@ -5,27 +5,50 @@
 #include "Snake.h"
 #include "Fruit.h"
 #include "Controller.h"
+#include <typeinfo>
 
-using namespace Data;
-
-
-Snake::Snake(QGraphicsScene* gameScene) : scene{ gameScene }, xDirection{ xVelocity }, yDirection{ 0 }
+Snake::Snake(QGraphicsScene* gameScene)
+    : head{Data::width / 2, Data::height / 2}, scene{ gameScene },
+      xDirection{ Data::xVelocity }, yDirection{ 0 },
+      toGrow { 0 }
 {
-    //setFocus();
-    setFlag(QGraphicsItem::ItemIsFocusable);
+
 }
+
 
 QPainterPath Snake::shape() const
 {
-    QPainterPath p;
-    p.setFillRule(Qt::WindingFill);
-    p.addEllipse(QPointF(0, 0), Data::snakeSize, Data::snakeSize);
-    return p;
+    QPainterPath painterPath;
+    painterPath.setFillRule(Qt::WindingFill);
+    painterPath.addEllipse(QPointF{0, 0}, Data::snakeSize, Data::snakeSize); // Snake's head is set to be an ellipse
+
+    for(auto point : tail) // Shape the tail
+    {
+        QPointF node = mapFromScene(point);
+        painterPath.addEllipse(QPointF(node.x(), node.y()), Data::snakeSize * 0.9, Data::snakeSize * 0.9); // Snake nodes are set to be smaller than the head
+    }
+    return painterPath;
 }
 
 QRectF Snake::boundingRect() const
 {
-    return QRectF(QPointF(0 - Data::snakeSize, 0 - Data::snakeSize), QPointF(0 + Data::snakeSize, 0 + Data::snakeSize));
+    qreal minX = head.x();
+    qreal minY = head.y();
+    qreal maxX = head.x();
+    qreal maxY = head.y();
+
+    for(auto point : tail) {
+        maxX = point.x() > maxX ? point.x() : maxX;
+        maxY = point.y() > maxY ? point.y() : maxY;
+        minX = point.x() < minX ? point.x() : minX;
+        minY = point.y() < minY ? point.y() : minY;
+    }
+
+    QPointF topLeft = mapFromScene(QPointF(minX, minY));
+    QPointF bottomRight = mapFromScene(QPointF(maxX, maxY));
+
+    return QRectF(QPointF(topLeft.x() - Data::snakeSize, topLeft.y() - Data::snakeSize),
+                  QPointF(bottomRight.x() - topLeft.x() + Data::snakeSize, bottomRight.y() - topLeft.y() + Data::snakeSize));
 }
 
 void Snake::paint(QPainter *painter, const QStyleOptionGraphicsItem *, QWidget *)
@@ -36,39 +59,41 @@ void Snake::paint(QPainter *painter, const QStyleOptionGraphicsItem *, QWidget *
     painter->restore();
 }
 
-void Snake::keyPressEvent(QKeyEvent* keyEvent)
+void Snake::move()
 {
-    switch (keyEvent->key()) {
-    case Qt::Key_Left:
-    {
-        //qDebug() << "Left";
-        moveLeft();
-        break;
-    }
-    case Qt::Key_Right:
-    {
-        //qDebug() << "Right";
-        moveRight();
-        break;
-    }
-    case Qt::Key_Up:
-    {
-        //qDebug() << "Up";
-        moveUp();
-        break;
-    }
-     case Qt::Key_Down:
-    {
-        //qDebug() << "Down";
-        moveDown();
-        break;
-    }
-    }
+    //QPointF lastHeadPosition{ head }; // Store the head position
+    //qDebug() << lastHeadPosition;
+    // Move the head forward
+    updateHead();
+    // Move the boyd forward
+
+
+   if(toGrow > 0)
+   {
+       QPointF newNode;
+       tail.push_back(newNode);
+       toGrow--;
+   }
+   else
+   {
+       if(!tail.isEmpty())
+       {
+           tail.removeFirst();
+           tail.push_back(head);
+       }
+   }
+
+   setPos(head);
+}
+
+void Snake::eatFruit()
+{
+    toGrow++;
 }
 
 bool Snake::wallHit()
 {
-    if(x() > Data::width|| x() < -Data::width|| y() > Data::height || y() < -Data::height)
+    if(x() > Data::width || x() < -Data::width || y() > Data::height || y() < -Data::height)
     {
         qDebug() << "Wall hit";
         return true;
@@ -81,32 +106,53 @@ void Snake::checkCollision()
     QList<QGraphicsItem*> collision = collidingItems();
     for(auto item : collision)
     {
-        scene->removeItem(item);
-        Controller::fruitsNumber--;
-        //snake->eat()
+        if(Fruit* fruit = dynamic_cast<Fruit*>(item))
+        {
+            eatFruit();
+            scene->removeItem(item);
+            Controller::fruitsNumber--;
+        }
     }
+
+    // todo: self eating
 }
 
 void Snake::moveLeft()
 {
-    xDirection = -1 * xVelocity;
+    xDirection = -Data::xVelocity;
     yDirection = 0;
+    updateHead();
 }
 
 void Snake::moveRight()
 {
-    xDirection = xVelocity;
+    xDirection = Data::xVelocity;
     yDirection = 0;
+    updateHead();
 }
 
 void Snake::moveUp()
 {
     xDirection = 0;
-    yDirection = -1 * yVelocity;
+    yDirection = -Data::yVelocity;
+    updateHead();
 }
 
 void Snake::moveDown()
 {
     xDirection = 0;
-    yDirection = yVelocity;
+    yDirection = Data::yVelocity;
+    updateHead();
+}
+
+void Snake::updateHead()
+{
+    head.setX(head.x() + xDirection);
+    head.setY(head.y() + yDirection);
+}
+
+void Snake::updateTailNode(QPointF& node)
+{
+    node.setX(node.x() + xDirection);
+    node.setY(node.y() + yDirection);
 }
--- Snake.h
+++ Snake.h
@@ -1,19 +1,20 @@
 #pragma once
 #include <QGraphicsItem>
 #include <QKeyEvent>
-#include "Constants.h"
 #include <QPainter>
+#include <QVector>
+#include "Constants.h"
 
 class Snake : public QGraphicsItem
 {
 public:
     Snake(QGraphicsScene*);
-
+    ~Snake() {}
     QPainterPath shape() const; // Set the shape of snake to circle
     QRectF boundingRect() const; // Set a rectangle to be the bouding shape of snake
     void paint(QPainter *painter, const QStyleOptionGraphicsItem *, QWidget *);
 
-    void eatFruit();
+    void eatFruit(); // Remove a fruit from the scene after colliding with it
     void addTail();
 
     bool wallHit(); // Determine if snake hit a wall
@@ -22,17 +23,25 @@ public:
     int getXDirection() const { return xDirection; }
     int getYDirection() const { return yDirection; }
 
+    void move(); // Move snake and its tail
 
+    QPointF getHead() { return head; }
     void moveLeft(); // Set xDirection to -xDirection and yDirection to 0
     void moveRight(); // Set xDireciton to +xDirection and yDirection to 0
     void moveUp(); // Set yDirection to -yDirection and xDirection to 0
     void moveDown(); // Set yDirection to +yDirection and xDirection to 0
 
 private:
+    QPointF head;
+    QVector<QPointF> tail; // Store all the exisiting points of snake's tail
     QGraphicsScene* scene = nullptr;
     int xDirection;
     int yDirection;
+    int snakeLength;
+    int toGrow;
 
-    void keyPressEvent(QKeyEvent*);
+    void updateHead();
+    void updateTailNode(QPointF&);
+
 };
 
