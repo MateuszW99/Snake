966908ef6abc4e02dbc170ed102e9e42fe2bb84e
Eklerek <mwiczuk99@gmail.com>
2019-11-26 23:43:38 +0100

    Added and removed some variables

--- Constants.h
+++ Constants.h
@@ -8,9 +8,14 @@ namespace Data {
     // Snake
     constexpr int snakeSize = 10;
     constexpr qreal snakeRadius = 3.0;
-    constexpr int yVelocity = 4;
-    constexpr int xVelocity = 4;
-    constexpr int SnakeLatencySpeed = 50;    
+    static int velocity = 4;
+    constexpr int snakeLatencySpeed = 50;
+    constexpr int maxSpeed = 12;
+    enum Direction
+    {
+        Left, Right, Up, Down
+    };
+
     //Fruit
     constexpr int FruitSpawnTime = 3000;
     constexpr int maxFruitNumber = 4;
