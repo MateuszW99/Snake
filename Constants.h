f50bc66f9737778e513db97bbc3b0b9d7a3308ab
Eklerek <mwiczuk99@gmail.com>
2019-11-27 22:11:08 +0100

    Lowered max speed

--- Constants.h
+++ Constants.h
@@ -10,15 +10,15 @@ namespace Data {
     constexpr qreal snakeRadius = 3.0;
     static int velocity = 4;
     constexpr int snakeLatencySpeed = 50;
-    constexpr int maxSpeed = 12;
+    constexpr int maxSpeed = 10;
     enum Direction
     {
         Left, Right, Up, Down
     };
 
     //Fruit
-    constexpr int FruitSpawnTime = 3000;
+    constexpr int FruitSpawnTime = 2500;
     constexpr int maxFruitNumber = 4;
-    constexpr int fruitSize = 10;
+    constexpr int fruitSize = 9;
     constexpr qreal fruitRadius = 3.0;
 }
